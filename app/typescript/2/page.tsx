b1a4589bc47e09c42e7c5d75243d0574d477bf6e
Riki Itokazu <itokazuriki@gmail.com>
2024-12-20 22:52:41 -0800

    generics part

--- app/typescript/2/page.tsx
+++ app/typescript/2/page.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 
+
 type Lengthwise = {
     length: number
 }
@@ -55,7 +56,46 @@ const x2 = createInstance(Bee).keeper.hasMask;
 const y2 = createInstance(Lion).keeper.nametag;
 
 // generic paramter defaults
+// declare function create(): Children<HTMLDivElement, HTMLDivElement[]>;
+// declare function create<T extends HTMLElement>(element: T): Container<T, T[]>;
+// declare function create<T extends HTMLElement, U extends HTMLElement>(
+//   element: T,
+//   children: U[]
+// ): Container<T, U[]>;
 
+// const create<T extends whatever = default if nothing specified>(p)
+// declare function create<T extends HTMLElement = HTMLDivElement, U extends HTMLElement[] = T[]>(
+//     element?: T,
+//     children?: U
+//   ): Container<T, U>;
+   
+// const div = create();
+
+
+// const p = create(new HTMLParagraphElement());
+const func = <T extends number>(a: number, b: number) => {
+    return a + b;
+}
+
+// contravaraince is when a function parameter is a subtype of the parameter type, like we can use 
+// Consumer<Cat> wherever Consumer<Animal> is expected. 
+
+// Contravariant annotation (input)
+type Consumer<in T> = {
+    consume: (arg: T) => void
+}
+//  Covariant annotation (output)
+type Producer<out T> = {
+    make(): () => T
+}
+
+// Invariant annotation
+type ProducerConsumer<in out T> = {
+    consume: (arg: T) => void; 
+    make(): T; 
+}
+
+       
 export default function Generics(): JSX.Element {
     return (
         <div className = 'p-5 grid grid-cols-2'>
